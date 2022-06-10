3e49e474c451592a0ab18f6decc4bd47e42295ca
timotheus ingga pranata <trancexe@gmail.com>
2022-06-10 15:38:22 +0900

    Update promise.js
    memanfaatkan asycn dan await

--- promise.js
+++ promise.js
@@ -1,7 +1,19 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emo) => {
+  try {
+    // ambil data dari promise
+    const theaterIXX = await promiseTheaterIXX();
+    const theaterVGC = await promiseTheaterVGC();
+    const allTheaters = [...theaterIXX, ...theaterVGC];
+    // filter berdasarkan emo
+    return allTheaters.filter((theater) => theater.hasil === emo).length;
+  }
+  catch (err) {
+    console.log(err);
+  };
+};
 
 module.exports = {
   promiseOutput,
